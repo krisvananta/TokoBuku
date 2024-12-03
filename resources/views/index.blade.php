15b9bb3dba54472b4d67138c54995db1487a9928
krisvananta <afifkrisvananta@gmail.com>
2024-12-03 07:05:37 +0700

    feat: minor bug fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.bootstrap5.css" />
     <link rel="stylesheet" href="{{ asset('/css/stylesheets.css') }}">
     <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
@@ -12,6 +13,7 @@
     <script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script>
     <script src="https://cdn.datatables.net/2.1.8/js/dataTables.bootstrap5.js"></script>
 </head>
+
 <body>
     @extends('auth.layouts')
     <div class="table-container" id="home">
@@ -26,36 +28,40 @@
                     <th>Harga</th>
                     <th>Tanggal Terbit</th>
                     <th>Status</th>
-                    @if(Auth::check() && Auth::user()->level=='admin')
+                    @if (Auth::check() && Auth::user()->level == 'admin')
                         <th>Aksi</th>
                         <th>Edit</th>
                     @endif
                 </tr>
             </thead>
             <tbody>
-                @foreach($data_buku as $index => $buku)
+                @foreach ($data_buku as $index => $buku)
                     <tr>
-                        <td>{{ $index+1 }}</td>
+                        <td>{{ $index + 1 }}</td>
                         <td>{{ $buku->judul }}</td>
                         <td>
-                            <img src="{{ asset('storage/TRUZ.jpeg')}}" alt="Foto" style="width: 100px; height: auto;">
+                            <img src="{{ asset('storage/bookcover.png') }}" alt="Foto"
+                                style="width: 100px; height: auto;">
                         </td>
                         <td>{{ $buku->penulis }}</td>
-                        <td>{{ "Rp. ".number_format($buku->harga, 2, ',', '.') }}</td>
-                        <td>{{\Carbon\Carbon::parse($buku->tgl_terbit)->format('d-m-Y') }}</td>
+                        <td>{{ 'Rp. ' . number_format($buku->harga, 2, ',', '.') }}</td>
+                        <td>{{ \Carbon\Carbon::parse($buku->tgl_terbit)->format('d-m-Y') }}</td>
                         <td>
-                            @if($buku->status == 'Tersedia')
+                            @if ($buku->status == 'Tersedia')
                                 <span class="badge badge-available">Tersedia</span>
                             @elseif($buku->status == 'Habis')
                                 <span class="badge badge-out-of-stock">Habis</span>
                             @endif
                         </td>
-                        @if(Auth::check() && Auth::user()->level=='admin')
+                        @if (Auth::check() && Auth::user()->level == 'admin')
                             <td>
-                                <form action="{{ route('buku.destroy', $buku->id) }}" method="POST" style="display:inline;">
+                                <form action="{{ route('buku.destroy', $buku->id) }}" method="POST"
+                                    style="display:inline;">
                                     @csrf
                                     @method('DELETE')
-                                    <button type="submit" onclick="return confirm('Anda yakin ingin menghapus buku ini?')" class="btn badge btn-danger btn-sm">Hapus</button>
+                                    <button type="submit"
+                                        onclick="return confirm('Anda yakin ingin menghapus buku ini?')"
+                                        class="btn badge btn-danger btn-sm">Hapus</button>
                                 </form>
                             </td>
                             <td>
@@ -70,9 +76,12 @@
                 @endforeach
             </tbody>
         </table>
-        <div class="d-flex justify-content-center mb-3 mt-3">
-            <a href="{{ route('buku.create') }}" class="btn badge btn-primary">Tambah Buku</a>
-        </div>
+        @if (Auth::check() && Auth::user()->level == 'admin')
+            <div class="d-flex justify-content-center mb-3 mt-3">
+                <a href="{{ route('buku.create') }}" class="btn badge btn-primary">Tambah Buku</a>
+            </div>
+        @endif
     </div>
 </body>
+
 </html>
